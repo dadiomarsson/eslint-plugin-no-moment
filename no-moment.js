735e53c7dcc835709b9e49a427615468faadaf24
Daði Ómarsson <dadio20@ru.is>
2024-04-13 17:53:52 +0000

    Change to no-moment.js and yml

--- no-moment.js
+++ no-moment.js
@@ -1,8 +1,28 @@
 module.exports = {
+    meta: {
+        type: 'problem',
+        docs: {
+            description: 'Moment library is not allowed',
+            category: 'Best Practices',
+            recommended: true
+        },
+        schema: [],
+        messages: {
+            noMomentLib: 'Moment library is not allowed'
+        }
+    },
     create(context) {
         return {
             ImportDeclaration(node) {
                 if (node.source.value === 'moment') {
+                    context.report({
+                        node,
+                        message: 'The use of moment is forbidden. Use date-fns instead.',
+                    });
+                }
+            },
+            CallExpression(node) {
+                if (node.callee.name === 'require' && node.arguments[0].value === 'moment') {
                     context.report({
                         node,
                         message: 'The use of moment is forbidden. Use date-fns instead.',
