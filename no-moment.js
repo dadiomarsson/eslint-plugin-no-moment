d8e86084771fafccbd5de9b2b98dc508ad31fcd4
Daði Ómarsson <dadio20@ru.is>
2024-04-14 15:39:09 +0000

    no-moment.js code layout change

--- no-moment.js
+++ no-moment.js
@@ -1,34 +1,37 @@
-module.exports = {
-    meta: {
-        type: 'problem',
-        docs: {
-            description: 'Moment library is not allowed',
-            category: 'Best Practices',
-            recommended: true
-        },
-        schema: [],
-        messages: {
-            noMomentLib: 'Moment library is not allowed'
-        }
+const meta = {
+    type: 'problem',
+    docs: {
+        description: 'Moment library is not allowed',
+        category: 'Best Practices',
+        recommended: true
     },
-    create(context) {
-        return {
-            ImportDeclaration(node) {
-                if (node.source.value === 'moment') {
-                    context.report({
-                        node,
-                        message: 'The use of moment is forbidden. Use date-fns instead.',
-                    });
-                }
-            },
-            CallExpression(node) {
-                if (node.callee.name === 'require' && node.arguments[0].value === 'moment') {
-                    context.report({
-                        node,
-                        message: 'The use of moment is forbidden. Use date-fns instead.',
-                    });
-                }
+    schema: [],
+    messages: {
+        noMomentLib: 'Moment library is not allowed'
+    }
+};
+function create(context) {
+    return {
+        ImportDeclaration(node) {
+            if (node.source.value === 'moment') {
+                context.report({
+                    node,
+                    message: 'The use of moment is forbidden. Use date-fns instead.',
+                });
+            }
+        },
+        CallExpression(node) {
+            if (node.callee.name === 'require' && node.arguments[0].value === 'moment') {
+                context.report({
+                    node,
+                    message: 'The use of moment is forbidden. Use date-fns instead.',
+                });
             }
         }
-    }
+    };
+}
+
+module.exports = {
+    meta: meta,
+    create: create
 };
